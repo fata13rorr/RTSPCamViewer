0cd95949a1f0a4c103e4784a08e42b98ed6828b0
AP <109635460+fata13rorr@users.noreply.github.com>
2025-08-02 17:31:12 -0400

    Add files via upload

---
+++ rtsp-multiview_macos/config.py
@@ -0,0 +1,6 @@
+RTSP_STREAMS = [
+    "rtsp://admin:password@192.168.1.101:554/Streaming/Channels/101",
+    "rtsp://admin:password@192.168.1.101:554/Streaming/Channels/102",
+    "rtsp://admin:password@192.168.1.101:554/Streaming/Channels/103",
+    "rtsp://admin:password@192.168.1.101:554/Streaming/Channels/104"
+]
